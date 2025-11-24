81857e5cbc4431d91cb24331fcde1af50f918dc0
jihyun0121 <gheun098@gmail.com>
2025-11-24 15:18:23 +0900

    설문 기본 dto 추가

---
+++ backend/src/main/java/com/survey/backend/dtos/FormDTO.java
@@ -0,0 +1,36 @@
+package com.survey.backend.dtos;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FormDTO {
+    @JsonProperty("form_id")
+    private Long formId;
+
+    @JsonProperty("form_title")
+    private String title;
+
+    @JsonProperty("form_description")
+    private String description;
+
+    @JsonProperty("is_public")
+    @Builder.Default
+    private Boolean isPublic = false;
+
+    @JsonProperty("user_id")
+    private Long userId;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
+}
