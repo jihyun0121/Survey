a15824ab07db43fa0282992d9711ea0fdc023b3a
jihyun0121 <gheun098@gmail.com>
2025-11-28 09:38:14 +0900

    설문 게시 버튼 아이콘 수정

--- frontend/src/components/editor/PublishButton.js
+++ frontend/src/components/editor/PublishButton.js
@@ -16,7 +16,7 @@ export default function PublishButton({ formId, initialPublic }) {
             await FormAPI.updateForm(formId, { is_public: nextState });
 
             setIsPublic(nextState);
-            alert(nextState ? "게시 완료!" : "게시 취소 완료!");
+            alert(nextState ? "게시 완료" : "게시 취소 완료");
         } catch (err) {
             console.error(err);
             alert("처리 중 오류가 발생했습니다.");
@@ -26,7 +26,7 @@ export default function PublishButton({ formId, initialPublic }) {
     return (
         <div className="dropdown">
             <button className="icon-btn" data-bs-toggle="dropdown" data-tooltip={isPublic ? "게시 취소" : "게시하기"}>
-                <i className="bi bi-upload"></i>
+                {isPublic ? <i class="bi bi-box-arrow-in-down"></i> : <i class="bi bi-box-arrow-up"></i>}
             </button>
 
             <div className="dropdown-menu small">
