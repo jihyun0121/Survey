9a6632a4dbb8e92085473cd10ce6ee017a19de69
jihyun0121 <gheun098@gmail.com>
2025-12-01 16:37:24 +0900

    css 클래스명 수정

--- frontend/src/components/response/ResponsesHeader.js
+++ frontend/src/components/response/ResponsesHeader.js
@@ -57,11 +57,11 @@ export default function ResponsesHeader({ Respondent, activeTab, onChangeTab, fo
 
                 <div className="d-flex gap-2">
                     <button className="icon-btn-sm" onClick={() => handleDownload("xls")} disabled={downloading} data-tooltip={"엑셀 다운로드"}>
-                        {downloading ? "다운로드 중..." : <i class="bi bi-file-earmark-spreadsheet-fill"></i>}
+                        {downloading ? "다운로드 중..." : <i className="bi bi-file-earmark-spreadsheet-fill"></i>}
                     </button>
 
                     <button className="icon-btn-sm" onClick={() => handleDownload("csv")} disabled={downloading} data-tooltip={"엑셀 다운로드"}>
-                        {downloading ? "다운로드 중..." : <i class="bi bi-filetype-csv"></i>}
+                        {downloading ? "다운로드 중..." : <i className="bi bi-filetype-csv"></i>}
                     </button>
                 </div>
             </div>
